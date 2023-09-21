9c142669fa5a83cb4190e89d3f10f30c40b74c8d
ruttklein <klein.rutto@student.moringaschool.com>
2023-09-21 21:49:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+// Define a sample employee object
+const employee = {
+    name: 'John',
+    age: 30,
+    role: 'Engineer',
+  }
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = { ...employee, [key]: value };
+    return updatedEmployee;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    const clonedEmployee = { ...employee };
+    delete clonedEmployee[key];
+    return clonedEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  module.exports = {
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey,
+  };
